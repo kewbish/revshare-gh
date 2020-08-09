81842c359e6499c84ea4c3daa29d1dd69b39934d
Kewbish <kewbish@gmail.com>
2020-08-09 14:25:52 -0700

    Implement scraper for funding fragment

--- revshare-crx/revshare.js
+++ revshare-crx/revshare.js
@@ -1,9 +1,20 @@
 const hasSponsor = document.getElementById("sponsor-button-repo");
 if (hasSponsor) {
     console.log("Revshare-CRX // Sponsored.");
+    const fundingUrl = `${location.href.split("/").splice(0, 5).join("/")}/funding_links?fragment=1`;
+    fetch(fundingUrl).then((res) => {
+        return res.text();
+    }).then((html) => {
+        const parser = new DOMParser();
+        const htmlNodes = parser.parseFromString(html, 'text/html');
+        const links = htmlNodes.querySelectorAll(".d-flex.mb-3>.flex-auto.min-width-0>a");
+        var walletLinks = [];
+        links.forEach((l) => {
+            if (l.innerText.startsWith("$")) {
+                walletLinks.push(l.innerText);
+            }
+        });
+    }).catch((err) => {
+        console.warn(`Revshare-CRX // Error: ${err}`);
+    });
 }
-
-const url = location.href.split("/").slice(3, 5);
-const defBranch = document.getElementById("branch-select-menu") ? document.getElementById("branch-select-menu").children[0].children[1].innerHTML : null;
-const repoSpon = `https://raw.githubusercontent.com/${url.join("/")}/${defBranch}/.github/FUNDING.yml`;
-const orgSpon = `https://raw.githubusercontent.com/${url[0]}/.github/${defBranch}/FUNDING.yml`
