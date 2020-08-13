5fe6f23bc1c495b4084d5bbec72f4322c9aeb144
Kewbish <kewbish@gmail.com>
2020-08-12 19:49:49 -0700

    Add comments

--- revshare-crx/revshare.js
+++ revshare-crx/revshare.js
@@ -7,29 +7,35 @@ chrome.storage.sync.get("starsOnly", (v) => {
 if (hasSponsor) {
     console.log("Revshare-CRX // Sponsored.");
     const fundingUrl = `${location.href.split("/").splice(0, 5).join("/")}/funding_links?fragment=1`;
+    // fetch url for fragment page including all funding links
     fetch(fundingUrl).then((res) => {
         return res.text();
     }).then((html) => {
         const parser = new DOMParser();
         const htmlNodes = parser.parseFromString(html, 'text/html');
+        // create node tree from text of fetched page
         const links = htmlNodes.querySelectorAll(".d-flex.mb-3>.flex-auto.min-width-0>a");
+        // select each link
         var walletLinks = [];
         links.forEach((l) => {
             if (l.innerText.startsWith("$")) {
                 walletLinks.push(l.innerText);
+                // if matches the wallet pointer format
             }
         });
-        const chosen = walletLinks[Math.floor(Math.random() * walletLinks.length)];
-        const monetizationTag = document.createElement('meta');
-        monetizationTag.name = 'monetization';
-        monetizationTag.content = chosen;
         if (starsOnly) {
             console.log("Revshare-CRX // Stars only.");
             var starredObj = document.getElementsByClassName("js-toggler-container js-social-container starring-container");
             starredObj = starredObj[0].innerText.split("\n")[0].trim();
+            // either 'unstar', therefore starred, or 'star', therefore unstarred
         }
         if (starsOnly && starredObj == "Unstar" || !starsOnly) {
+            const chosen = walletLinks[Math.floor(Math.random() * walletLinks.length)];
+            const monetizationTag = document.createElement('meta');
+            monetizationTag.name = 'monetization';
+            monetizationTag.content = chosen;
             document.head.appendChild(monetizationTag);
+            // form meta tag, and append
         }
     }).catch((err) => {
         console.error(`Revshare-CRX // Error: ${err}`);
