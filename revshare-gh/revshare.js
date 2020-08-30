a8048a09bcdb1a00189ec789f9ed8dbb627bdccb
Kewbish <kewbish@gmail.com>
2020-08-29 19:29:11 -0700

    Format, catches

--- revshare-gh/revshare.js
+++ revshare-gh/revshare.js
@@ -13,7 +13,7 @@ var walletLinks = [];
 var allDepUrls = [];
 
 // url
-const url = window.location.href.toString(); 
+const url = window.location.href.toString();
 
 function createMeta(array) {
     const chosen = array[Math.floor(Math.random() * array.length)];
@@ -25,12 +25,16 @@ function createMeta(array) {
 }
 
 function getFundingLinks() {
-   return fetch('https://api.github.com/graphql', {
+    return fetch('https://api.github.com/graphql', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${globalThis.key}` },
-        body: JSON.stringify({ query: `{ repository(name: "${url.split("/")[4]}", owner: "${url.split("/")[3]}")  { fundingLinks { url } } }` }),
-    }).then((res) => res.json()
-    ).then((res) => {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${globalThis.key}`
+        },
+        body: JSON.stringify({
+            query: `{ repository(name: "${url.split("/")[4]}", owner: "${url.split("/")[3]}")  { fundingLinks { url } } }`
+        }),
+    }).then((res) => res.json()).then((res) => {
         const links = res.data.repository.fundingLinks;
         // select each link
         links.forEach((l) => {
@@ -52,58 +56,61 @@ function getFundingLinks() {
 function getDepLinks() {
     const url = window.location.href.toString();
     return fetch('https://api.github.com/graphql', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json', 'Accept': 'application/vnd.github.hawkgirl-preview+json', 'Authorization': `Bearer ${globalThis.key}` },
-                body: JSON.stringify({ query: `{ repository(name: "${url.split("/")[4]}", owner: "${url.split("/")[3]}") {dependencyGraphManifests { nodes { dependencies { nodes { repository { fundingLinks { url } } } } } } } }` }),
-            })
-                .then(res => res.json())
-                .then(res => {
-                    console.log("Revshare-CRX // Data: ", res);
-                    const allDepNodes = res.data.repository.dependencyGraphManifests.nodes;
-                    if (allDepNodes.length > 0) {
-                        allDepNodes.forEach((el) => {
-                            allDepUrlLists = el.dependencies.nodes;
-                            allDepUrlNodes = [];
-                            allDepUrlLists.forEach((el) => {
-                                el.forEach((subEl) => {
-                                    if (subEl.repository != null) {
-                                        allDepUrlNodes.push(subEl.repository.fundingLinks);
-                                    }
-                                });
-                            });
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/vnd.github.hawkgirl-preview+json',
+                'Authorization': `Bearer ${globalThis.key}`
+            },
+            body: JSON.stringify({
+                query: `{ repository(name: "${url.split("/")[4]}", owner: "${url.split("/")[3]}") {dependencyGraphManifests { nodes { dependencies { nodes { repository { fundingLinks { url } } } } } } } }`
+            }),
+        })
+        .then(res => res.json())
+        .then(res => {
+            console.log("Revshare-CRX // Data: ", res);
+            const allDepNodes = res.data.repository.dependencyGraphManifests.nodes;
+            if (allDepNodes.length > 0) {
+                allDepNodes.forEach((el) => {
+                    allDepUrlLists = el.dependencies.nodes;
+                    allDepUrlNodes = [];
+                    allDepUrlLists.forEach((el) => {
+                        el.forEach((subEl) => {
+                            if (subEl.repository != null) {
+                                allDepUrlNodes.push(subEl.repository.fundingLinks);
+                            }
                         });
-                        allDepUrls = [];
-                        allDepUrlNodes.forEach((el) => {
-                            el.forEach((subEl) => {
-                                if (subEl.url.startsWith("$")) {
-                                    allDepUrls.push(subEl.url);
-                                }
-                            });
-                        });
-                    }
+                    });
                 });
-             
+                allDepUrls = [];
+                allDepUrlNodes.forEach((el) => {
+                    el.forEach((subEl) => {
+                        if (subEl.url.startsWith("$")) {
+                            allDepUrls.push(subEl.url);
+                        }
+                    });
+                });
+            }
+        });
+
 }
 
 if (hasSponsor) {
     console.log("Revshare-GH // Sponsored");
     getFundingLinks().then(() => {
         const sponsorMeta = Math.round(Math.random());
-	    console.log(`Revshare-GH // Dependency? ${sponsorMeta}`);
+        console.log(`Revshare-GH // Dependency? ${sponsorMeta}`);
         if (sponsorMeta) {
             getDepLinks().then(() => {
                 if (allDepUrls) {
                     createMeta(allDepUrls);
-                }
-                else if (walletLinks) {
+                } else if (walletLinks) {
                     // fallback when dependencies empty
                     createMeta(walletLinks);
                 }
-            });            
-        }
-        else if (walletLinks) {
+            });
+        } else if (walletLinks) {
             createMeta(walletLinks);
         }
     });
 }
-
