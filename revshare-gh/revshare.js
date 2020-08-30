ae96d1377e965ddad0f4ae06af30ef717bb3ed6d
Kewbish <kewbish@gmail.com>
2020-08-29 17:01:27 -0700

    Restructure

--- revshare-gh/revshare.js
+++ revshare-gh/revshare.js
@@ -8,19 +8,32 @@ chrome.storage.sync.get("starsOnly", (v) => {
     console.log("Revshare-GH // Executed.");
 });
 
-if (hasSponsor) {
-    console.log("Revshare-GH // Sponsored.");
-    const url = window.location.href.toString();
-    // fetch url for fragment page including all funding links
-    fetch('https://api.github.com/graphql', {
+// links variables
+var walletLinks = [];
+var allDepUrls = [];
+
+// url
+const url = window.location.href.toString(); 
+
+function createMeta(array) {
+    const chosen = array[Math.floor(Math.random() * array.length)];
+    const monetizationTag = document.createElement('meta');
+    monetizationTag.name = 'monetization';
+    monetizationTag.content = chosen;
+    document.head.appendChild(monetizationTag);
+    // form meta tag, and append
+}
+
+function getFundingLinks() {
+   return fetch('https://api.github.com/graphql', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${globalThis.key}` },
         body: JSON.stringify({ query: `{ repository(name: "${url.split("/")[4]}", owner: "${url.split("/")[3]}")  { fundingLinks { url } } }` }),
     }).then((res) => res.json()
     ).then((res) => {
+        console.log("Worked.");
         const links = res.data.repository.fundingLinks;
         // select each link
-        var walletLinks = [];
         links.forEach((l) => {
             if (l.url.startsWith("$")) {
                 walletLinks.push(l.url);
@@ -33,10 +46,13 @@ if (hasSponsor) {
             starredObj = starredObj[0].innerText.split("\n")[0].trim();
             // either 'unstar', therefore starred, or 'star', therefore unstarred
         }
-        const sponsorMeta = Math.round(Math.random());
-	console.log(`Revshare-GH // Dependency? ${sponsorMeta}`);
-        if (sponsorMeta) {
-            fetch('https://api.github.com/graphql', {
+    });
+
+}
+
+function getDepLinks() {
+    const url = window.location.href.toString();
+    return fetch('https://api.github.com/graphql', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json', 'Accept': 'application/vnd.github.hawkgirl-preview+json', 'Authorization': `Bearer ${globalThis.key}` },
                 body: JSON.stringify({ query: `{ repository(name: "${url.split("/")[4]}", owner: "${url.split("/")[3]}") {dependencyGraphManifests { nodes { dependencies { nodes { repository { fundingLinks { url } } } } } } } }` }),
@@ -57,7 +73,7 @@ if (hasSponsor) {
                                 });
                             });
                         });
-                        var allDepUrls = [];
+                        allDepUrls = [];
                         allDepUrlNodes.forEach((el) => {
                             el.forEach((subEl) => {
                                 if (subEl.url.startsWith("$")) {
@@ -66,29 +82,29 @@ if (hasSponsor) {
                             });
                         });
                     }
-                    if (allDepUrls) {
-                        createMeta(allDepUrls);
-                    }
-                    else if ((starsOnly && starredObj == "Unstar" || !starsOnly) && walletLinks.length > 0) {
-                        createMeta(walletLinks);
-                    	// fallback for when dependencies don't have wallet links
-		    }
-                })
-                .catch(err => console.error(`Revshare-GH // Error: ${err}`));
+                });
+             
+}
+
+if (hasSponsor) {
+    console.log("Revshare-GH // Sponsored");
+    getFundingLinks.then(() => {
+        const sponsorMeta = Math.round(Math.random());
+	    console.log(`Revshare-GH // Dependency? ${sponsorMeta}`);
+        if (sponsorMeta) {
+            getDepLinks.then(() => {
+                if (allDepUrls) {
+                    createMeta(allDepUrls);
+                }
+                else if (walletLinks) {
+                    // fallback when dependencies empty
+                    createMeta(walletLinks);
+                }
+            });            
         }
-        else if ((starsOnly && starredObj == "Unstar" || !starsOnly) && walletLinks.length > 0) {
+        else if (walletLinks) {
             createMeta(walletLinks);
         }
-    }).catch((err) => {
-        console.error(`Revshare-GH // Error: ${err}`);
     });
 }
 
-function createMeta(array) {
-    const chosen = array[Math.floor(Math.random() * array.length)];
-    const monetizationTag = document.createElement('meta');
-    monetizationTag.name = 'monetization';
-    monetizationTag.content = chosen;
-    document.head.appendChild(monetizationTag);
-    // form meta tag, and append
-}
