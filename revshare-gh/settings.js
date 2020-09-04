2f3c9a704a22d495b11be3dd6f4d50c261351e55
Kewbish <kewbish@gmail.com>
2020-09-04 16:29:03 -0700

    Add submit to form

--- revshare-gh/settings.js
+++ revshare-gh/settings.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     chrome.storage.sync.get("starsOnly", (v) => {
         document.getElementById("stars-only").checked = v.starsOnly;
     });
+    document.getElementById("pat-form").addEventListener("submit", setPat);
     document.getElementById("pat").addEventListener("blur", setPat);
     chrome.storage.sync.get("pat", (pat) => {
         if (pat) {
@@ -14,11 +15,20 @@ document.addEventListener("DOMContentLoaded", () => {
 function setStar() {
     const checked = document.getElementById("stars-only");
     console.log(checked.checked);
-    chrome.storage.sync.set({ starsOnly: checked.checked }, () => {
-    });
+    chrome.storage.sync.set({
+        starsOnly: checked.checked
+    }, () => {});
 }
 
 function setPat() {
     const token = document.getElementById("pat");
-    chrome.storage.sync.set({ pat: token.value }, () => {});
+    chrome.storage.sync.set({
+        pat: token.value
+    }, () => {});
+    chrome.storage.sync.get("pat", (pat) => {
+        if (pat) {
+            document.getElementById("pat").value = pat.pat;
+        }
+    });
+
 }
